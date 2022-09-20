8ebd4e6405871edec51b9b62aee92b35b351fd5f
Suadabd <112632719+Suadabd@users.noreply.github.com>
2022-09-20 12:29:51 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log("Test");
+/**
+ * Name field.
+ */
+ document.getElementById("name").focus();
+/**
+ "Job Role" section
+ Job Role" section
+The "Job Role" section has an <input type="text"> field where users can enter a custom job role. If the user selects "Other" in the "Job Role" drop down menu, they can enter info into the "Other job role" text field. But this field should be hidden by default and only displayed once users select "Other" in the drop down menu, and be hidden if the user selects any other option.
+
+Hide the "text field" with the id of "other-job-role" so it is not displayed when the form first loads.
+Program the "Job Role" <select> element to listen for user changes. When a change is detected, display/hide the "text field" based on the user’s selection in the drop down menu.
+  */
+***//input type="text" name="other-job-role" id="other-job-role" class="other-job-role" placeholder="Other job role?">
+        ///</fieldset>
+
+ const tex
